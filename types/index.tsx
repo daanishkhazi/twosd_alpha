ee7c332c094f85073374bacfd1a6a8dac421683f
s-sub <shiv97@gmail.com>
2022-11-09 18:37:11 -0500

    Added subject selection and integrated API request/response

--- types/index.tsx
+++ types/index.tsx
@@ -1,4 +1,13 @@
 export interface Prompt {
     description: string;
+    gpt3_prefix: string;
+    placeholder_text: string;
+    output_limit: number;
     char_limit: number;
+    // TODO: Add API parameters needed for a given prompt set here
+}
+
+export interface PromptSet {
+    prompts: Array<Prompt>
+    // TODO: Add API parameters needed for a given prompt set here
 }
