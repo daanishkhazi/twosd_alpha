4531bc4ceabddd7972e60fb7af3564192df8db94
s-sub <shiv97@gmail.com>
2022-11-09 19:24:36 -0500

    Added typing for Subjects

--- types/index.tsx
+++ types/index.tsx
@@ -4,12 +4,15 @@ export interface Prompt {
     placeholder_text: string;
     output_limit: number;
     char_limit: number;
-    // TODO: Add API parameters needed for a given prompt set here
 }
 
 export interface PromptSet {
     prompts: Array<Prompt>
-    // TODO: Add API parameters needed for a given prompt set here
+}
+
+export interface Subject {
+    name: string;
+    subject_prefix: string;
 }
 
 export interface PromptSet {
