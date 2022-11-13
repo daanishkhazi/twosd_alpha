06befd0cdece8842aac5fd028039162b60b5cd16
s-sub <106557016+s-sub@users.noreply.github.com>
2022-11-13 16:44:54 -0500

    Account Options Extension (#12)
    * Refactored components from interface page
    
    * Added Google & Fb to nextauth
    
    * Added account dropdown, migrated tokenBalance/tokenQuota to be stored in session object

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,23 @@
 import { NextApiHandler } from "next";
-import NextAuth from "next-auth";
+import NextAuth, {Session, User} from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import prisma from "../../../lib/prisma";
+import { userAgent } from "next/server";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
+
+const getTokenCountAndQuota = async (session: Session, user: User) => {
+    const DBuser = await prisma.user.findUnique({
+        where: {
+            email: user.email as string,
+          },
+    })
+    return {tokenBalance: user.tokensUsed, tokenQuota: user.tokenQuota}
+};
 
 const options = {
   providers: [
@@ -24,6 +34,16 @@ const options = {
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
     })
   ],
+  callbacks: {
+    // "any" typing used since Next Auth module augmentation not working properly - see issue #5576 on NextAuth GitHub
+    async session({session, user}: {session: Session, user: User}) {
+      const {tokenBalance: tokenBalanceFromDB, tokenQuota: tokenQuotaFromDB} = await getTokenCountAndQuota(session, user) 
+      session.user.tokensUsed = tokenBalanceFromDB;
+      session.user.tokenQuota = tokenQuotaFromDB;
+      console.log('sess', session)
+      return session
+    }
+  },
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
