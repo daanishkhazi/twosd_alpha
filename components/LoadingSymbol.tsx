375b8515a6889a5388a6c7e1044b63ab5bc6921f
s-sub <106557016+s-sub@users.noreply.github.com>
2022-11-18 17:32:13 -0500

    UI updates2 (#22)
    * Refactored components from interface page
    
    * Added Google & Fb to nextauth
    
    * logo animated for loading icon
    
    * Interface UI overhaul
    
    * loading symbol parameters fixed
    
    * more ui updates for interface page
    
    * ui changes

--- components/LoadingSymbol.tsx
+++ components/LoadingSymbol.tsx
@@ -4,11 +4,13 @@ const LoadingSymbol = (props: { color: string }) => {
   const color = props.color;        
     return (
       <div className="flex fill-current">
-        <svg className="h-full" viewBox="0 0 488 488" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect className={styles.iconElement1} width="231.325" height="231.325" fill={color}/>
-          <rect className={styles.iconElement2} x="256.675" y="256.675" width="231.325" height="231.325" fill={color}/>
-          <rect className={styles.iconElement3} y="256.675" width="231.325" height="231.325" rx="115.662" fill={color}/>
-          <rect className={styles.iconElement4} x="256.675" width="231.325" height="231.325" fill={color}/>
+        <svg className="h-full" viewBox="0 0 510 510" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g className={styles.iconfull} >
+            <rect width="231.325" height="231.325" fill={color}/>
+            <rect x="256.675" y="256.675" width="231.325" height="231.325" fill={color}/>
+            <rect y="256.675" width="231.325" height="231.325" rx="115.662" fill={color}/>
+            <rect x="256.675" width="231.325" height="231.325" fill={color}/>
+          </g>
         </svg>
       </div>
     )
