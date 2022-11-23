ee8ac9d41252020d1c452b127cac79015fcc5430
Daanish Khazi <dkhazi@gmail.com>
2022-11-22 18:59:17 -0800

    analytisc (#34)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { BalanceCtx } from "../Context/balance-context";
 import { useState } from "react";
 import { promptContextValue } from "../types";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [promptBalance, setPromptBalance] = useState<promptContextValue>({
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <SessionProvider session={pageProps.session}>
       <BalanceCtx.Provider value={{ promptBalance, setPromptBalance }}>
         <Component {...pageProps} />
+        <Analytics />
       </BalanceCtx.Provider>
     </SessionProvider>
   );
